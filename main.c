befbcf767753d000957eb000088df0a68caf4667
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-04-07 10:59:50 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int first ,secand,sum;
+    printf("Enter two no \n");
+    scanf("%d%d",&first,&secand);
+    sum=first+secand;
+    printf("sum of two no is = %d",sum);
+    return 0;
+}
